8b95e89711b660abc70b66a89b52067af367e961
Olafur Waage <olafurw@gmail.com>
2018-08-28 16:25:45 +0200

    Adding chrono parsing to sched event.

--- sched_event.cpp
+++ sched_event.cpp
@@ -1,5 +1,18 @@
 #include "sched_event.h"
 
+#include <iomanip>
+#include <sstream>
+
+TimePoint
+StringToTimePoint(
+    const std::string&  aTimeStr)
+{
+    std::stringstream ss(aTimeStr);
+    std::tm tm = {};
+    ss >> std::get_time(&tm, "%Y-%m-%d %H:%M:%S");
+    return std::chrono::system_clock::from_time_t(std::mktime(&tm));
+}
+
 SchedEvent::SchedEvent(
     const nlohmann::json& aEventJson)
 {
@@ -12,4 +25,7 @@ SchedEvent::SchedEvent(
     myEventStartStr = aEventJson["event_start"].get<std::string>();
     myEventEndStr = aEventJson["event_end"].get<std::string>();
     myGoers = std::stoi(aEventJson["goers"].get<std::string>());
+
+    myEventStart = StringToTimePoint(myEventStartStr);
+    myEventEnd = StringToTimePoint(myEventEndStr);
 }
--- sched_event.h
+++ sched_event.h
@@ -2,7 +2,10 @@
 #define CPPCON_ROOM_ASSIGNMENT_SCHED_EVENT_H
 
 #include <string>
+#include <chrono>
 #include "json.hpp"
+
+using TimePoint = std::chrono::system_clock::time_point;
 
 struct SchedEvent
 {
@@ -10,7 +13,9 @@ struct SchedEvent
     std::string     myName = "";
     std::string     mySpeakers = "";
     std::string     myEventStartStr = "";
+    TimePoint       myEventStart{};
     std::string     myEventEndStr = "";
+    TimePoint       myEventEnd{};
     int32_t         myGoers = 0;
 
     SchedEvent() = default;
